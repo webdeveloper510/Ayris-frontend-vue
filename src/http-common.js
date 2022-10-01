3ae421272d00246f2deee156868f3c52a27ab1c7
Kajals22 <kajal@codenomad.net>
2022-10-01 16:11:42 +0530

    build

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_SERVER = "http://localhost:8000/";
+const API_SERVER = "http://54.167.85.36:8000/";
 
 const apiClient = axios.create({
   baseURL: API_SERVER,
